a5c81605037f1796373aac8d6216412a1e00afc2
YI-Hashimoto <hashimoto_satoshi@y-i-group.co.jp>
2026-02-22 16:13:00 +0900

    add:CodeBuild検証資材の追加

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import type { Request, Response } from "express";
+
+const app = express();
+const port = Number(process.env.PORT ?? 3000);
+
+app.get("/",(_req:Request,res:Response) => {
+    const now = new Date().toISOString();
+    res.status(200).json({
+        status:"ok!",
+        message:"Hello World!",
+        currentTime:now,
+    });
+});
+
+app.listen(port,"0.0.0.0",() => {
+    console.log(`Server is started : http://0.0.0.0:${port}`);
+});
