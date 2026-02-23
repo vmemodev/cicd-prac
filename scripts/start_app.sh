f53f5d4512e4cfd2c2ebdcc080563862e9d7b3a6
YI-Hashimoto <hashimoto_satoshi@y-i-group.co.jp>
2026-02-23 14:28:28 +0900

    fix:shファイル一部修正、appspec.yml追記

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+# コマンドが1つでも失敗したらスクリプト全体を終了する
 set -e
+# アプリディレクトリへ移動
 cd /opt/myapp
 
+# Node.jsがインストールされているか確認
+# command -v node ⇒ nodeコマンドが存在するか確認するコマンド
+# >/dev/null 2>&1 ⇒ エラー出力を表示しない（静かに確認）
 if ! command -v node >/dev/null 2>&1; then
   echo "Node.js not found. Install Node.js first." >&2
   exit 1
 fi
 
+# pm2 があるか確認
 if ! command -v pm2 >/dev/null 2>&1; then
   npm i -g pm2
 fi
 
-pm2 start dist/server.js --name myapp --update-env
+# アプリ起動
+# dist/index.js を pm2 で起動
+pm2 start dist/index.js --name myapp --update-env
+# PM2のプロセスリストを保存
 pm2 save
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,10 @@ app.get("/",(_req:Request,res:Response) => {
     });
 });
 
+app.get("/health", (_req, res) => {
+    res.status(200).send("OK");
+});
+
 app.listen(port,"0.0.0.0",() => {
     console.log(`Server is started : http://0.0.0.0:${port}`);
 });
