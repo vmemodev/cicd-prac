42a21dc952168465b60dc02a4bdd211e89480afc
YI-Hashimoto <hashimoto_satoshi@y-i-group.co.jp>
2026-03-08 14:17:01 +0900

    fix: バージョン表記追記

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import express from 'express';
+import express from "express";
 import type { Request, Response } from "express";
 
 const app = express();
 const port = Number(process.env.PORT ?? 3000);
 
-app.get("/",(_req:Request,res:Response) => {
-    const now = new Date().toISOString();
-    res.status(200).json({
-        status:"ok!",
-        message:"Hello World!",
-        currentTime:now,
-    });
+app.get("/", (_req: Request, res: Response) => {
+  const now = new Date().toISOString();
+  res.status(200).json({
+    status: "ok!",
+    message: "Hello World! v1.0",
+    currentTime: now,
+  });
 });
 
 app.get("/health", (_req, res) => {
-    res.status(200).send("OK");
+  res.status(200).send("OK");
 });
 
-app.listen(port,"0.0.0.0",() => {
-    console.log(`Server is started : http://0.0.0.0:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is started : http://0.0.0.0:${port}`);
 });
